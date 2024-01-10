59ae0eb9a751aee33f0a2343c1eea9e4af5ad984
Chaitanya Ashtekar <ashtekarchaitanya8@gmail.com>
2024-01-11 00:58:57 +0530

    Streamlit UI code

---
+++ UI/Home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import sys
+import os
+
+st.title("Open Source Intelligence")
+
+st.markdown(
+    "Open Source Intelligence refers to getting all the open source information about a particular individual. AI tools have made it possible to automate a bulk of this process to scrape information about a lot of people, fast."
+)
+
+st.image("download.png")
+
+st.markdown("The aim of this application is to use basic information like Name and a Picture of a person and fetch as much information as possible using Machine Learning models and AI tools.")
+
+st.markdown("With this we can demonstrate how AI can be useful/dangerous in OSINT and can have a discussion about the usage of such tools (targeted advertisement, online surveillance etc.)")
